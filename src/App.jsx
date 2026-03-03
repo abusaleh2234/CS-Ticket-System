4b2117e58cfbd2005863d8600420f65f59c43cf4
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-03-03 21:38:01 +0600

    Navbar and banner complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css'
+import Banner from './Component/Banner/Banner'
 import Navbar from './Component/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <div className="">
       <Navbar></Navbar>
+      <div className="bg-[#F5F5F5] ">
+        <div className=" max-w-7xl mx-auto">
+            <Banner></Banner>
+
+            <div className="">
+              
+            </div>
+        </div>
+        
       </div>
     </>
   )
